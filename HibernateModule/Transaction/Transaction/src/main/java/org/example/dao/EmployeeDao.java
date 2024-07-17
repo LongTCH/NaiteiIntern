0e9d1bd18b1d8a92769c229d3272d1e37d0e4a28
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-07-17 10:43:57 +0700

    transaction

---
+++ HibernateModule/Transaction/Transaction/src/main/java/org/example/dao/EmployeeDao.java
@@ -0,0 +1,21 @@
+package org.example.dao;
+
+import org.example.model.Employee;
+import org.example.util.HibernateUtil;
+
+
+public class EmployeeDao {
+    public void saveEmployee(Employee employee) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(employee);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+}
